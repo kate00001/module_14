7eb70f7bec63aa1019dc666018185ccee6883632
Kate <166659076+kate00001@users.noreply.github.com>
2024-12-20 21:08:44 +0300

    crud_functions.py

---
+++ crud_functions.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+
+conn = sqlite3.connect('bot_database.db')
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Products (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+)
+""")
+
+conn.commit()
+
+cursor.execute("SELECT id, title, description, price FROM Products")
+products = cursor.fetchall()
+
+Products = [
+    ('Продукт 1', 'Описание продукта 1', 100),
+    ('Продукт 2', 'Описание продукта 2', 200),
+    ('Продукт 3', 'Описание продукта 3', 300),
+    ('Продукт 4', 'Описание продукта 4', 400),
+    ]
+
+cursor.executemany("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)", Products)
+conn.commit()
+
+def get_all_products():
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, description, price FROM Products")
+    products = cursor.fetchall()
+    conn.close()
+    return products
+
+def remove_duplicates():
+    conn = sqlite3.connect('bot_database.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    DELETE FROM Products
+    WHERE id NOT IN (
+        SELECT MIN(id)
+        FROM Products
+        GROUP BY title, description, price
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+
+conn.close()
